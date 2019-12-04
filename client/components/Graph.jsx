d5c8a036bf47eb6fef77500c876a99e050919b8f
Aston Khor <Astonkhor@chenas-MacBook-Pro.local>
2019-12-03 17:06:28 -0800

    change chart id to fix chart

--- client/components/Graph.jsx
+++ client/components/Graph.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import ChartTab from './ChartTab.jsx';
 import Expand from './Expand.jsx';
 import Wrapper from '../styled-components/Wrapper'; 
-import { Chart } from '../styled-components/graph.js';
 
 const Graph = (props) => {
 	return (
 		<Wrapper.Graph>
-			<Chart />
+			<div id="stockPriceHistoryChart"></div>
 			<ChartTab changeView={props.changeView} view={props.state.view} />
 			<Expand />
 		</Wrapper.Graph>
