2d8bb040fe8a6fb957ffaedb65da8e636f931738
Aston Khor <Astonkhor@chenas-MacBook-Pro.local>
2019-12-05 13:48:54 -0800

    minor changes to Expand formatting

--- client/components/Expand.jsx
+++ client/components/Expand.jsx
@@ -4,7 +4,7 @@ import Wrapper from '../styled-components/Wrapper'
 var Expand = (props) => {
 	return (
 		<Wrapper.Expand>
-			<div>Expand &emsp; &emsp;</div>
+			Expand &emsp; &emsp;
 		</Wrapper.Expand>
 	)
 }
