ab90dbb86b006bd7eb99b55f70a2bdfcef12a260
Hyungjin Lee <leehyungjin4u@gmail.com>
2020-01-04 12:12:40 -0800

    Improve testing, server
    Created five pools instead of one alternating pool. Created load balance artillery script. Created pgbench script.

--- Database/postgresqldatabase.js
+++ Database/postgresqldatabase.js
@@ -1,30 +1,53 @@
 const psqlpassword = require('../keys/psqlrootpw');
 const { Pool } = require('pg');
 const moment = require('moment');
-let pool;
+const poolAB = new Pool({
+  user: 'root',
+  password: psqlpassword,
+  host: '172.31.9.30',
+  database: 'rhgraph'
+});
+const poolCD = new Pool({
+  user: 'root',
+  password: psqlpassword,
+  host: '172.31.3.116',
+  database: 'rhgraph'
+});
+const poolEF = new Pool({
+  user: 'root',
+  password: psqlpassword,
+  host: '172.31.2.235',
+  database: 'rhgraph'
+});
+const poolGH = new Pool({
+  user: 'root',
+  password: psqlpassword,
+  host: '172.31.4.3',
+  database: 'rhgraph'
+});
+const poolIJ = new Pool({
+  user: 'root',
+  password: psqlpassword,
+  host: '172.31.0.188',
+  database: 'rhgraph'
+});
+
 const poolProcessor = (ticker) => {
-  let host;
   if (ticker < 'C') {
-    host = '172.31.9.30';
+    return poolAB;
   } else if (ticker < 'E') {
-    host = '172.31.3.116';
+    return poolCD;
   } else if (ticker < 'G') {
-    host = '172.31.2.235';
+    return poolEF;
   } else if (ticker < 'I') {
-    host = '172.31.4.3';
+    return poolGH;
   } else {
-    host = '172.31.0.188';
+    return poolIJ;
   }
-  pool = new Pool({
-    user: 'root',
-    password: psqlpassword,
-    host: host,
-    database: 'rhgraph'
-  });
 }
 
 module.exports.getSymbolInfo = (ticker, cb) => {
-  poolProcessor(ticker);
+  const pool = poolProcessor(ticker);
   pool
     .query(`SELECT * FROM businesses WHERE symbol='${ticker}'`)
     .then(res => {
@@ -36,7 +59,7 @@ module.exports.getSymbolInfo = (ticker, cb) => {
 }
 
 module.exports.getTimeFrame = (ticker, timeframe, cb) => {
-  poolProcessor(ticker);
+  const pool = poolProcessor(ticker);
   pool
     .query(`SELECT * FROM prices_${timeframe} WHERE ticker='${ticker}' ORDER BY timest ASC`)
     .then(res => {
@@ -48,7 +71,7 @@ module.exports.getTimeFrame = (ticker, timeframe, cb) => {
 }
 
 module.exports.addOnePoint = (ticker, timeframe, time, price, cb) => {
-  poolProcessor(ticker);
+  const pool = poolProcessor(ticker);
   pool
     .query(`INSERT INTO prices_${timeframe} (ticker, price, timest) values ('${ticker}', ${price}, '${time}')`)
     .then(res => {
@@ -60,7 +83,7 @@ module.exports.addOnePoint = (ticker, timeframe, time, price, cb) => {
 }
 
 module.exports.updatePrice = (ticker, price, cb) => {
-  poolProcessor(ticker);
+  const pool = poolProcessor(ticker);
   pool
     .query(`UPDATE businesses SET price=${price} WHERE symbol=${ticker}`)
     .then(res => {
@@ -72,7 +95,7 @@ module.exports.updatePrice = (ticker, price, cb) => {
 }
 
 module.exports.deletePrice = (ticker, timeframe, time, cb) => {
-  poolProcessor(ticker);
+  const pool = poolProcessor(ticker);
   let timest = moment(time).format('YYYY-MM-DD HH:mm');
   pool
     .query(`DELETE FROM prices_${timeframe} WHERE timest='${timest}:00' AND ticker=${ticker}`)
